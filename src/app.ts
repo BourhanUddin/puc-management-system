2cadab26165fbac32fed7d019f96ed25d513177b
Hemel Bourhan <hemelbourhan@gmailgit config --global user.email Bourhan>
2024-07-08 23:56:30 +0600

    Your descriptive commit message

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import cors from 'cors';
+import express, { Request, Response } from 'express';
+const app = express();
+
+//parsers
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+export default app;
